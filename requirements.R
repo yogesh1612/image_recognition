209f99e7bc251149b80a040650cea819bacb73cb
Yogesh Khandelwal <yogesh1612@users.noreply.github.com>
2020-11-21 11:28:28 +0530

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,8 +1,14 @@
-install.packages("shiny")
-library("shiny")
+# install tesnorflow backend
 install.packages("tensorflow")
 library(tensorflow)
 install_tensorflow()
+
+# install keras 
 install.packages("keras")
 library("keras")
+
+# install other required packages
+install.packages("shiny")
 install.packages("wordcloud")
+install.packages("DT")
+
